2abdbd4f2490edec6dc881f974b150e2fe761d2a
James M. Allen <james.m.allen@gmail.com>
2015-10-21 11:28:01 -0400

    Tying in Contact model to api

--- contacts321/contacts321/urls.py
+++ contacts321/contacts321/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from contacts.views import ContactViewSet
+
+router = routers.DefaultRouter()
+router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(router.urls)),
 ]
